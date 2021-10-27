e46b155f83edbf66751fddae88d7d1104b67825b
davidRdlP <darope19@gmail.com>
2021-10-27 12:38:29 +0200

    limitacion de decimales

--- Eclipse/src/ejerciciosTema1/Ejercicio1.java
+++ Eclipse/src/ejerciciosTema1/Ejercicio1.java
@@ -1,9 +1,18 @@
 package ejerciciosTema1;
-
+import java.text.DecimalFormat;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		int numero = 2;
+		double numero1 = 2.268;
+		double numero2 = 4.51654;
+		double resultado;
+
+		DecimalFormat df = new DecimalFormat("#.00");
+
+		resultado = numero1*numero2;
+		System.out.println(df.format(resultado));
+
+		
 
 	}
 
